3a9260a29025a0cb6c913076f97a97a894f46481
Levon Hakopyan <levon.hakopyan@gmail.com>
2015-03-31 22:34:20 +0400

    initial commit, add plugin file

---
+++ wp-valid-cookie.php
@@ -0,0 +1,33 @@
+
+<?php
+	/*
+	Plugin Name: wp-valid-cookie
+	Plugin URI: https://github/lev09/wp-valid-cookie
+	Description: Plugin to check if cookies are valid send back curent user information.
+	Version: 1.0
+	Author: Levon Hakopyan
+	Author URI: https://github/lev09
+	License: GPL2
+	*/
+	//@include "../../../wp-load.php";
+
+	@include "../../../wp-load.php";
+
+	if (!empty($_COOKIE)) {
+	
+		if (is_user_logged_in()) {
+			$user = wp_get_current_user();
+			$logged_user["id"] = $user->data->ID;
+			$logged_user["login"] = $user->data->user_login;
+			$logged_user["roles"] = $user->roles;
+			print_r(json_encode($logged_user));
+			exit();
+		}
+	
+		$response = array("code"=>401, "status"=>"unauthorized");
+		print_r(json_encode($response));
+
+	}
+
+?>
+
